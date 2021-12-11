7fb2e315a0c41fa27b242c273f705eb37a72fe4b
Ciel85 <michaelwalter299@gmail.com>
2021-12-11 02:17:33 +0100

    home screen

---
+++ screens/TabOneScreen.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+
+import {Text, View, StyleSheet, Image, FlatList} from 'react-native';
+import ChatRoomItem from '../components/ChatRoomItem';
+
+import chatRoomsData from '../assets/dummy-data/ChatRooms';
+
+export default function TabOneScreen() {
+  return (
+    <View style={styles.page}>
+        <FlatList 
+        data={chatRoomsData}
+        renderItem={({ item }) => <ChatRoomItem chatRoom={item}/>}
+        showsVerticalScrollIndicator = {false}
+        /> 
+    </View>
+    
+  
+  );
+}
+
+const styles = StyleSheet.create({
+  page: {
+    backgroundColor:'white',
+    flex:1
+  }
+});
